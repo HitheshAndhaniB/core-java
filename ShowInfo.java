00df22f67bde45f3a1e7b8614a3844cb192408d1
HitheshAndhaniB <149232848+HitheshAndhaniB@users.noreply.github.com>
2023-11-08 11:59:17 +0530

    ShowInfo

---
+++ ShowInfo.java
@@ -0,0 +1,16 @@
+void ShowInfo(){
+	
+	System.out.println("Starting Showinfo in agent"); 
+	openTent(tent);
+	closeTent(tent);
+	
+	System.out.println("The id is :"+id); 
+	
+	System.out.println("Ending Showinfo in agent"); 
+	
+	
+	
+}
+
+
+}
